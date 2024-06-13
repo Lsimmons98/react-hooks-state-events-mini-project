f35fe29890adba03bf76c47ff7b353f422cd3675
Liam Simmons <liam.simmons@powerhrg.com>
2024-06-13 10:20:35 -0400

    l

--- src/components/CategoryFilter.js
+++ src/components/CategoryFilter.js
@@ -1,10 +1,22 @@
 import React from "react";
 
-function CategoryFilter() {
+function CategoryFilter({categories, filterTasks}) {
+
+  const handleClick = (e) => {
+    e.preventDefault()
+    e.target.classList.contains('selected') ? e.target.classList.remove('selected') : e.target.classList.add('selected')
+    const taskID = e.target.id
+    filterTasks(taskID)
+  }
+
   return (
     <div className="categories">
       <h5>Category filters</h5>
-      {/* render <button> elements for each category here */}
+      <button id={categories[0]} onClick={handleClick}>{categories[0]}</button>
+      <button id={categories[1]} onClick={handleClick}>{categories[1]}</button>
+      <button id={categories[2]} onClick={handleClick}>{categories[2]}</button>
+      <button id={categories[3]} onClick={handleClick}>{categories[3]}</button>
+      <button id={categories[4]} onClick={handleClick}>{categories[4]}</button>
     </div>
   );
 }
--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,9 +1,29 @@
-import React from "react";
+import React, {useState} from "react";
+import Task from './Task'
+import CategoryFilter from './CategoryFilter'
 
-function TaskList() {
+function TaskList({tasks}) {
+  const [displayedTasks, editDisplayedTasks] = useState([...tasks])
+
+  const deleteTask = (taskID) => {
+    const filteredTasks = displayedTasks.filter(task => task.text !== taskID)
+    editDisplayedTasks(filteredTasks)
+  }
+
+  const filterByCategory = (taskID) => {
+      const filteredTasks = displayedTasks.filter(task => task.category === taskID)
+      editDisplayedTasks(filteredTasks)
+  }
+
+<CategoryFilter filterTasks={filterByCategory}/>
+
+  const showTasks = () =>{
+   return  displayedTasks.map((task) => <Task text={task.text} key={task.text }category={task.category} filterTasks={deleteTask}/>)
+  }
+
   return (
     <div className="tasks">
-      {/* display a list of tasks using Task component */}
+      {showTasks()}
     </div>
   );
 }
